01473c009ce27f28260f63a9373999b59d708849
Nahom Asrat Begashaw <nahomcookies@gmail.com>
2020-06-26 01:46:42 +0300

    Add an ArrayList to use for Displaying words on NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,11 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,5 +15,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");   words.add("two");
+        words.add("three");   words.add("four");
+        words.add("five");   words.add("six");
+        words.add("seven");   words.add("eight");
+        words.add("nine");   words.add("ten");
+
+        LinearLayout numbersView = (LinearLayout) findViewById(R.id.numbersView);
+
+        for (int i=0; i<words.size(); i++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            numbersView.addView(wordView);
+        }
     }
 }
