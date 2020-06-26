48e7b8454178d66922d9f642e2c28fd4e94625db
Nahom Asrat Begashaw <nahomcookies@gmail.com>
2020-06-26 17:43:39 +0300

    Add words into each activity as list_item views and use a ListView in combination with a custom ArrayAdapter to recycle views that contain custom design

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red","weṭeṭṭi"));
+        words.add(new Word("green","chokokki"));
+        words.add(new Word("brown","ṭakaakki"));
+        words.add(new Word("gray","ṭopoppi"));
+        words.add(new Word("white","kululli"));
+        words.add(new Word("black","kelelli"));
+        words.add(new Word("dusty yellow","ṭopiisә"));
+        words.add(new Word("mustard yellow","chiwiiṭә"));
+
+        WordAdapter familyAdapter = new WordAdapter(this, 0, words);
+        ListView familyListView = (ListView) findViewById(R.id.colors_list);
+        familyListView.setAdapter(familyAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,19 +18,20 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("one");   words.add("two");
-        words.add("three");   words.add("four");
-        words.add("five");   words.add("six");
-        words.add("seven");   words.add("eight");
-        words.add("nine");   words.add("ten");
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'aacha"));
 
-        LinearLayout numbersView = (LinearLayout) findViewById(R.id.numbersView);
-
-        for (int i=0; i<words.size(); i++){
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            numbersView.addView(wordView);
-        }
+        WordAdapter wordAdapter = new WordAdapter(this, 0, words);
+        ListView listView = (ListView) findViewById(R.id.numbers_list);
+        listView.setAdapter(wordAdapter);
     }
 }
