065795d3aae79508aa3463022fb321a53ac50a2a
Nahom Asrat Begashaw <nahomcookies@gmail.com>
2020-06-26 19:43:52 +0300

    Change the layout of list item to have an image. Add placeholder image and test app. Import assets for adding into list items later.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,17 +4,16 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImage;
 
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    public String getDefaultTranslation(){
-        return mDefaultTranslation;
-    }
+    public String getDefaultTranslation(){return mDefaultTranslation;}
 
-    public String getMiworkTranslation(){
-        return mMiwokTranslation;
-    }
+    public String getMiwokTranslation(){return mMiwokTranslation;}
+
+    public int getImageResourceID(){return  mImage;}
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,10 +27,12 @@ public class WordAdapter extends ArrayAdapter<Word> {
         }
 
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-        miwokTextView.setText(currentWord.getMiworkTranslation());
+        miwokTextView.setText(currentWord.getMiwokTranslation());
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
 
         return listItemView;
     }
